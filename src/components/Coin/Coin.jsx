708805b768cc0118e6e9d7692932f64c76d6f5fb
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-09-18 22:56:12 -0700

    events
    events errors

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -13,33 +13,10 @@ export default class Coin extends Component {
     }
     this.handleClick = this.handleClick(this); // this binding gives us access to handleClick, this.setState...
   }
-  /*
-  componentDidMount() {
-    const callback = () => {
-      // set the state to a new random value
-      // we allow one percent upward change
-      //const randomPercentage = 1.0 + Math.random() * 0.01;
-      // here we allow a range up and down change
-      const randomPercentage = 0.995 + Math.random() * 0.01;
-      // DON'T DO THIS: (react won't allow this.state variable to be used more than once) hence you only use it in the constructor
-      // this.state.price = this.state.price * randomPercentage;
-      this.setState( function(oldState){
-        return {
-          price: oldState.price * randomPercentage
-        };
-      });
-      // simpler code below but preferec code above, using a function
-      //this.setState({price: oldState.price * randomPercentage})
-    }
-    setInterval(callback, 1000);
-    //setInterval ( () => console.log('done'), 1000 ) instructor input in console of explorer
-  }
-  */
+  
  handleClick(e) {
    // prevent form from reloading
-   (e).preventDefault();
-   console.log("you clicked");
-   
+    e.preventDefault();
    const randomPercentage = 0.995 + Math.random() * 0.01;
    this.setState(function(oldState){
     return {
