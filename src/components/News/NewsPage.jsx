2eae9caf0fe28e35443612930ec40599ba5790f6
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-12-27 20:09:06 -0800

    news search
    news drop-down is almost working. The drop-down shows the coins from api but when onChange event, the selected coin does not get filtered properly

--- src/components/News/NewsPage.jsx
+++ src/components/News/NewsPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import News from './News';
 import useFetch from "../Utility/useFetch";
 import styled from 'styled-components';
+import coinGecko from '../Utility/coinGecko';
 
 const StyledHeaderNews = styled.header`  
   width: 100%;
@@ -15,13 +16,15 @@ const StyledHeaderNews = styled.header`
 
 function NewsPage() {
   //-------news----------------
-  const newsCatergory = 'cryptocurrency';
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const [coinListData, setCoinListData] = useState([]);
 
+  const searchCrypto = newsCategory;
   const options = {
     method: 'GET',
     url: 'https://bing-news-search1.p.rapidapi.com/news/search',
     params: {
-      q: newsCatergory,
+      q: searchCrypto,
       count: '20',
       freshness: 'Week',
       textFormat: 'Raw',
@@ -42,9 +45,40 @@ function NewsPage() {
   // <h1>{joke?.setup} : {joke?.delivery}</h1>
   const { data: getNews, loading, error, datePublished } = useFetch(options);
 
-  if (loading) return <h4 style={{color: 'grey'}}>Loading...</h4>;
+  async function getCoinList() {
+    try {
+      const res = await coinGecko.get('coins');
+      //console.log("response:", res);
+      setCoinListData(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  if (error) return <h4 style={{color: "darkorange"}}>Error...</h4>
+
+  useEffect(() => {
+    getCoinList();
+    return () => {
+      console.log("cleanup")
+    }
+  }, []) // component did mount
+
+  const filterOption = coinListData.filter((option) => {
+    return option.name === coinListData;
+  });
+
+
+  useEffect(() => {
+    console.log("filter: ", filterOption);
+  }, [filterOption]);
+
+  console.log("getCoinList: ", coinListData);
+
+
+  if (loading) return <h4 style={{ color: 'grey' }}>News Loading...</h4>;
+
+  if (error) return <h4 style={{ color: "darkorange" }}>News loading: Error...</h4>
+
 
   //------news--end-----------------
 
@@ -57,6 +91,21 @@ function NewsPage() {
       <br />
       <br />
       <div className='row'>
+        {coinListData && (
+          <div className="form-group">
+            <label for="exampleSelect1" className="form-label mt-4"><h2>Select a Crypto</h2></label>
+            <select
+              className="form-select" style={{ width: "50%" }}
+              id="exampleSelect1"
+              ofilterOption
+              value={newsCategory}
+              onChange={(e) => setNewsCategory(e.target.value)}
+            >
+              {coinListData.map((currency) => <option value={currency.name}>{currency.name}</option>)}
+            </select>
+
+          </div>
+        )}
         {getNews && getNews.value.map((news, index) => {
           return (
             <News key={index}
